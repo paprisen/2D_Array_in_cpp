c73f51bfa3b449bd9ecdccf4ef98514d38d9f38f
Papri <papri.sen2020@gmail.com>
2025-08-10 13:13:05 +0530

    Update Exp8_1_Matrix(2DArray).cpp

--- Exp8_1_Matrix(2DArray).cpp
+++ Exp8_1_Matrix(2DArray).cpp
@@ -1,50 +1,57 @@
 // Papri Sen
 // 24070123070
 // A3
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-    int marks[3][3];
-    int i, j;
-    for(i=0;i<3;i++) {
-        for(j=0;j<3;j++) {
-            cout << "Enter element ("<<i<<","<<j<<"):";
-            cin >> marks[i][j];
+
+int main(){
+    int x, y;
+
+    cout<<"Enter the number of rows: ";
+    cin>>x;
+
+    cout<<"Enter the number of columns: ";
+    cin>>y;
+
+    int mat[x][y];
+
+    cout<<"Enter elements of Matrix: "<<endl;
+
+    for(int i=0; i<x; i++){
+        for(int j=0; j<y; j++){
+            cin>>mat[i][j];
         }
     }
-    cout<<"matrix is:";
-    for(i=0;i<3;i++) {
-        for(j=0;j<3;j++) {
-            cout<<marks[i][j];
+    cout<<" Matrix is:"<<endl;
+    for(int i=0; i<x; i++){
+        for(int j=0; j<y; j++){
+            cout<<mat[i][j]<<"\t";
         }
-        cout << endl;
+        cout<<endl;
     }
 
+
     return 0;
+
 }
 
 /*
 Output:
-Enter element (0,0):
+Enter the number of rows: 3
+Enter the number of columns: 3
+Enter elements of Matrix: 
 1
-Enter element (0,1):
 2
-Enter element (0,2):
 3
-Enter element (1,0):
 4
-Enter element (1,1):
 5
-Enter element (1,2):
 6
-Enter element (2,0):
 7
-Enter element (2,1):
 8
-Enter element (2,2):
 9
-matrix is:
-123
-456
-789
+Matrix is:
+1	2	3	
+4	5	6	
+7	8	9
 */
+
